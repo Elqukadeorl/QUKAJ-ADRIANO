ccf6cdad81e21644965682f19e0d3549d4db30a6
Edoardo Genova <genova.edoardo@gmail.com>
2019-05-31 08:48:31 +0200

    COMPITO GIT QUKAJ

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+     int contatore = 1;
+     while(contatore <= 10){
+               cout << " il contatore mostra: " << contatore << endl;
+               contatore++;
+               };
+}
